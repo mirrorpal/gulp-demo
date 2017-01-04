67f2bb50f6213e6c02df5c27fe9948f7e5681261
周志豪 <363420113@qq.com>
2017-01-04 21:05:53 +0800

    update

---
+++ dist/js/common.js
@@ -0,0 +1,24 @@
++ function($){
+    "use strict";
+    var timeout;
+    $.sidePanel = function(){
+        clearTimeout(timeout);
+        $(".side-panel").show();
+        timeout = setTimeout(function(){
+            $(".side-panel-content").addClass('side-show');
+            $(".side-overlay").addClass('weui_mask_visible');
+        },50);
+    }
+    $(document).on("tap",".side-overlay,.side-close",function(){
+        $(".side-panel-content").removeClass('side-show');
+        $(".side-overlay").removeClass('weui_mask_visible');
+        timeout = setTimeout(function(){
+            $(".side-panel").hide();
+        },200);
+    });
+}($);
+
++ function($){
+    "use strict";
+    var a = 1;
+}($);
--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,10 @@ var gulp = require('gulp'),
     sass = require('gulp-sass'),
     minifycss = require('gulp-minify-css'),
     rename = require('gulp-rename'),
+    uglify = require('gulp-uglify'),
+    concat = require('gulp-concat'),
     notify = require('gulp-notify'),
+    ejs = require("gulp-ejs"),
     browserSync = require('browser-sync').create(),
     del = require('del');
 
@@ -16,14 +19,35 @@ gulp.task('styles', function() {
     .pipe(notify({ message: 'Styles task complete' }))
     .pipe(browserSync.stream());
 });
+gulp.task('js',function(){
+    return gulp.src([
+        'src/js/a.js',
+        'src/js/b.js'
+    ])
+    .pipe(concat('common.js'))
+    .pipe(gulp.dest('dist/js'))
+    .pipe(notify({ message: 'js task complete' }));
+});
+gulp.task('uglify', ['js'], function() {
+  return gulp.src(['dist/js/*.js', '!dist/js/*.min.js'])
+    .pipe(rename({suffix: '.min'}))
+    .pipe(uglify({}))
+    .pipe(gulp.dest('dist/js'));
+});
+gulp.task('ejs', function () {
+  return gulp.src(["src/view/*.html", "!src/view/_*.html"])
+    .pipe(ejs({}))
+    .pipe(gulp.dest("dist/view"));
+});
 gulp.task('clean', function() {
-    del(['dist/css'])
+    del(['dist/css','dist/js'])
 });
 gulp.task('serve', ['styles'], function() {
-        browserSync.init({
-            server: "./src/view"
-        });
-        gulp.watch('src/css/*.scss', ['styles']);
-        gulp.watch("src/view/*.html").on('change', browserSync.reload);
-   });
-gulp.task('default', ['clean','serve']);
+    browserSync.init({
+        server: "./dist/view"
+    });
+    gulp.watch('src/css/*.scss', ['styles']);
+    gulp.watch('src/js/*.js', ['uglify']);
+    gulp.watch("src/view/*.html",['ejs']).on('change', browserSync.reload);
+});
+gulp.task('default', ['clean','ejs','uglify','serve']);
